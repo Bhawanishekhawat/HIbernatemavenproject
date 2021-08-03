664f59e8adc65538c1f6e0a95fab7bd0c271b3e4
m45termin6 <m45termin6@gmail.com>
2021-08-03 13:39:37 +0530

    fixing of merge issue

--- src/main/java/com/jcg/hibernate/maven/dao/StudentDaoImpl.java
+++ src/main/java/com/jcg/hibernate/maven/dao/StudentDaoImpl.java
@@ -10,18 +10,11 @@ public class StudentDaoImpl implements StudentDao{
 
 	public void saveStudent(Student student) {
 		
-<<<<<<< HEAD
-		Session session = AppMain.getSession();
-		Transaction transaction = session.beginTransaction();
-		session.save(student);
-		transaction.commit();
-		
-=======
 		Session session =  AppMain.getSession();
 		Transaction transaction = session.beginTransaction();
 		session.save(student);
 		transaction.commit();
->>>>>>> c9ce8a4cf74078c894b645cd631c77c93ca58127
+
 	}
 
 }
--- src/main/java/com/jcg/hibernate/maven/dao/UserDaoImpl.java
+++ src/main/java/com/jcg/hibernate/maven/dao/UserDaoImpl.java
@@ -2,29 +2,17 @@ package com.jcg.hibernate.maven.dao;
 
 import org.hibernate.Session;
 import org.hibernate.Transaction;
-<<<<<<< HEAD
-
-=======
->>>>>>> c9ce8a4cf74078c894b645cd631c77c93ca58127
 import com.jcg.hibernate.maven.AppMain;
 import com.jcg.hibernate.maven.User;
 
 public class UserDaoImpl implements UserDao {
 
 	public void saveuser(User user) {
-<<<<<<< HEAD
 		Session session =AppMain.getSession();
 		Transaction transaction = session.beginTransaction();
 		session.save(user);
 		transaction.commit();
 		
-=======
-		Session session =  AppMain.getSession();
-		Transaction transaction = session.beginTransaction();
-		session.save(user);
-		transaction.commit();
-	
->>>>>>> c9ce8a4cf74078c894b645cd631c77c93ca58127
 	}
 
 }
